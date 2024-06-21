7fba4a38ddcab605bae9231643df75a6442c87c0
james-squire <james@squire.uk>
2024-06-21 10:46:28 +0100

    Update dark-mode-toggle.js

--- scripts/dark-mode-toggle.js
+++ scripts/dark-mode-toggle.js
@@ -36,12 +36,12 @@ function colorModeToggle() {
   let lightColors = {};
   let darkColors = {};
   cssVariables.split(",").forEach(function (item) {
-    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let lightValue = computed.getPropertyValue(`--light--${item}`);
     let darkValue = computed.getPropertyValue(`--dark--${item}`);
     if (lightValue.length) {
       if (!darkValue.length) darkValue = lightValue;
-      lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
+      lightColors[`--light--${item}`] = lightValue;
+      darkColors[`--light--${item}`] = darkValue;
     }
   });
 
